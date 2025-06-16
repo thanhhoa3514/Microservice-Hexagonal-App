9dd2619f4232bad8fed51bab46e05c52d82027f2
thanhhoa3514 <ngothanhhoa3514@gmail.com>
2025-06-16 23:31:34 +0700

    feat : create interface to connect two thread of the hexagon structure

---
+++ src/modules/category/interface/index.ts
@@ -0,0 +1,25 @@
+import { Pagination } from "../../../share/model/paging";
+import { CategoryConditionDTO, CategoryCreateDTO, CategoryUpdateDTO } from "../model/category-dto";
+import { Category } from "../model/category-model";
+
+export interface ICategoryUseCase {
+    createNewCategory(data: CategoryCreateDTO): Promise<string>;
+    updateCategory(id: string, data: CategoryUpdateDTO): Promise<void>;
+    deleteCategory(id: string): Promise<void>;
+    getDetailCategory(id: string): Promise<Category>;
+    listCategory(pagination: Pagination, condition: CategoryConditionDTO): Promise<{ categories: Category[], pagination: Pagination }>;
+}
+
+export interface IRepository extends IQueryRepository, ICommandRepository {
+
+}
+export interface IQueryRepository {
+    getById(id: string): Promise<Category>;
+    getAll(pagination: Pagination, condition: CategoryConditionDTO): Promise<{ categories: Category[], pagination: Pagination }>;
+}
+
+export interface ICommandRepository {
+    update(id: string, data: CategoryUpdateDTO): Promise<void>;
+    delete(id: string): Promise<void>;
+    insert(data: Category): Promise<void>;
+}
