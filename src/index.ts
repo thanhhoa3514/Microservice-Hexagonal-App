43e472ac907fcd1bd5824ccbaaab70c05a818a39
thanhhoa3514 <ngothanhhoa3514@gmail.com>
2025-06-16 23:35:18 +0700

    feat : update the index.ts file with the hexagonal handle api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from "express";
+
 import dotenv from "dotenv";
-import { setUpCategoryAPIModule } from "./modules/category";
+import { setUpCategoryAPIHex } from "./modules/category";
 dotenv.config();
 const express = require("express");
 import { sequelize } from "./share/component/sequelize";
@@ -11,7 +11,7 @@ import { sequelize } from "./share/component/sequelize";
     const port = process.env.PORT || 3000;
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-    app.use('/v1', await setUpCategoryAPIModule(sequelize));
+    app.use('/v1', await setUpCategoryAPIHex(sequelize));
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
     });
