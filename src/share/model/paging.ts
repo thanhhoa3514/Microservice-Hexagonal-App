01c31918d362af62f6386f9021a07f8abc484969
thanhhoa3514 <ngothanhhoa3514@gmail.com>
2025-06-18 23:22:11 +0700

    feat : update a paging share component

--- src/share/model/paging.ts
+++ src/share/model/paging.ts
@@ -2,14 +2,15 @@ import z from "zod";
 
 export const PaginationSchema = z.object({
     page: z.coerce.number().int().positive().min(1).default(1),
-    limit: z.coerce.number().int().positive().min(1).max(100).default(10)
+    limit: z.coerce.number().int().positive().min(1).max(100).default(10),
+    total: z.coerce.number().int().positive().min(0).default(0)
 })
 
 export type Pagination = z.infer<typeof PaginationSchema>;
 
 export const Paging = (data: Pagination) => {
-    const { page, limit } = data;
+    const { page, limit, total } = data;
     const offset = (Number(page) - 1) * Number(limit);
-    return { offset, limit };
+    return { offset, limit, total };
 }
 
