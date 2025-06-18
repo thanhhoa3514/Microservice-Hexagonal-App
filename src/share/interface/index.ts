24531455c667d810b95fa67e2db3396bf8c92cfb
thanhhoa3514 <ngothanhhoa3514@gmail.com>
2025-06-18 23:27:11 +0700

    feat : create a model status shared component file and change somethings not matter

--- src/share/interface/index.ts
+++ src/share/interface/index.ts
@@ -5,7 +5,7 @@ export interface IRepository<Entity, Condition, UpdateDTO> extends IQueryReposit
 }
 export interface IQueryRepository<Entity, Condition> {
     getById(id: string): Promise<Entity>;
-    getAll(pagination: Pagination, condition: Condition): Promise<{ categories: Entity[], pagination: Pagination }>;
+    getAll(pagination: Pagination, condition: Condition): Promise<{ entities: Entity[], pagination: Pagination }>;
 }
 
 export interface ICommandRepository<Entity, UpdateDTO> {
---
+++ src/share/model/mode-status.ts
@@ -0,0 +1,5 @@
+export enum EntityStatus {
+    ACTIVE = "ACTIVE",
+    INACTIVE = "INACTIVE",
+    DELETED = "DELETED"
+}
