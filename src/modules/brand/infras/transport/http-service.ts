84b56ef8fd5549c3e0d29d788abd86df36b8df5b
thanhhoa3514 <ngothanhhoa3514@gmail.com>
2025-06-18 23:27:44 +0700

    feat : create a protocol handle file for calling API

---
+++ src/modules/brand/infras/transport/http-service.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from "express";
+import { IBrandUseCase } from "../../interface";
+
+import { BrandCreateDTOSchema, BrandUpdateDTOSchema } from "../../model/brand.dto";
+import { PaginationSchema } from "../../../../share/model/paging";
+
+export class BrandHttpService {
+    constructor(private readonly useCase: IBrandUseCase) {
+
+    }
+    async createAPI(req: Request, res: Response): Promise<void> {
+        try {
+            const { success, data, error } = BrandCreateDTOSchema.safeParse(req.body);
+            if (!success) {
+                res.status(400).json({ message: "Invalid request body", error: error.message });
+                return;
+            }
+            const id = await this.useCase.createNewBrand(data);
+            res.status(201).json({ message: "Brand created successfully", data: { id } });
+        } catch (error) {
+            res.status(500).json({ message: "Internal server error", error: error });
+        }
+
+    }
+    async updateAPI(req: Request, res: Response): Promise<void> {
+        try {
+            const { success, data, error } = BrandUpdateDTOSchema.safeParse(req.body);
+            if (!success) {
+                res.status(400).json({ message: "Invalid request body", error: error.message });
+                return;
+            }
+            await this.useCase.updateBrand(req.params.id, data);
+            res.status(200).json({ message: "Brand updated successfully" });
+        } catch (error) {
+            res.status(500).json({ message: "Internal server error", error: error });
+        }
+    }
+    async deleteAPI(req: Request, res: Response): Promise<void> {
+        try {
+            await this.useCase.deleteBrand(req.params.id);
+            res.status(200).json({ message: "Brand deleted successfully" });
+        } catch (error) {
+            res.status(500).json({ message: "Internal server error", error: error });
+        }
+    }
+    async getDetailAPI(req: Request, res: Response): Promise<void> {
+        try {
+            const brand = await this.useCase.getDetailBrand(req.params.id);
+            res.status(200).json({ message: "Brand retrieved successfully", data: brand });
+        } catch (error) {
+            res.status(500).json({ message: "Internal server error", error: error });
+        }
+    }
+    async listAPI(req: Request, res: Response): Promise<void> {
+        try {
+            const { success, data, error } = PaginationSchema.safeParse(req.query);
+            if (!success) {
+                res.status(400).json({ message: "Invalid request body", error: error.message });
+                return;
+            }
+            const { brands, pagination } = await this.useCase.listBrand(data, req.query);
+            res.status(200).json({ message: "Brand retrieved successfully", data: { brands, pagination } });
+        } catch (error) {
+            res.status(500).json({ message: "Internal server error", error: error });
+        }
+    }
+}   
