4e812652dac056919d9625c5d0c2077746deef12
thanhhoa3514 <ngothanhhoa3514@gmail.com>
2025-06-20 21:30:46 +0700

    fix : change the require paging model

--- src/share/model/paging.ts
+++ src/share/model/paging.ts
@@ -3,7 +3,7 @@ import z from "zod";
 export const PaginationSchema = z.object({
     page: z.coerce.number().int().positive().min(1).default(1),
     limit: z.coerce.number().int().positive().min(1).max(100).default(10),
-    total: z.coerce.number().int().positive().min(0).default(0)
+    total: z.coerce.number().int().min(0).default(0)  // Removed .positive() to allow 0
 })
 
 export type Pagination = z.infer<typeof PaginationSchema>;
