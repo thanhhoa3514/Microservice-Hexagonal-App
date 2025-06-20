f03566e2c771fa9a90e2c12f48a7f43395435b32
thanhhoa3514 <ngothanhhoa3514@gmail.com>
2025-06-20 23:35:02 +0700

    refactor : we don't want to inject a class usecase in transport layer so we have to contact through interface command handle

---
+++ src/modules/brand/usecase/create-new-brand.ts
@@ -0,0 +1,41 @@
+import { v7 } from "uuid";
+import { CreateCommand, IBrandRepository, IBrandUseCase, ICreateNewBrandCommandHandler } from "../interface";
+import { BrandAlreadyExistsError, BrandValidationError } from "../model/brand-error";
+import { BrandCreateDTOSchema } from "../model/brand.dto";
+import { Brand } from "../model/brand-model";
+import { BrandStatus } from "../model/brand-enum";
+
+export class CreateNewBrandCommandHandler implements ICreateNewBrandCommandHandler<CreateCommand, string> {
+    constructor(private readonly brandRepository: IBrandRepository) {
+    }
+
+    async execute(data: CreateCommand): Promise<string> {
+        // Validate the actual brand data (data.cmd)
+        const validation = BrandCreateDTOSchema.safeParse(data.cmd);
+        if (!validation.success) {
+            throw new BrandValidationError("createData", data.cmd, validation.error.message);
+        }
+
+        // Check if brand with same name already exists
+        const existingBrand = await this.brandRepository.findByCondition({ name: data.cmd.name });
+        if (existingBrand) {
+            throw new BrandAlreadyExistsError(data.cmd.name);
+        }
+
+        const id = v7();
+        const brandData: Brand = {
+            id: id,
+            name: data.cmd.name,
+            image: data.cmd.image,
+            description: data.cmd.description,
+            tagLine: data.cmd.tagLine,
+            status: BrandStatus.ACTIVE,
+            created_at: new Date(),
+            updated_at: new Date()
+        }
+
+        await this.brandRepository.insert(brandData);
+        return id;
+    }
+
+} 
