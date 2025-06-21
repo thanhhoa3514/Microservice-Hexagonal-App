ce1eea34c83c02212b61c345f4a7da43735c1eaa
thanhhoa3514 <ngothanhhoa3514@gmail.com>
2025-06-21 17:57:12 +0700

    refactor : create a shared use case and  CRQS handle

--- src/share/interface/index.ts
+++ src/share/interface/index.ts
@@ -13,3 +13,19 @@ export interface ICommandRepository<Entity, UpdateDTO> {
     delete(id: string): Promise<void>;
     insert(data: Entity): Promise<void>;
 }
+export interface ICreateNewBrandCommandHandler<Command, Result> {
+    execute(data: Command): Promise<Result>;
+}
+export interface IQueryHandler<Query, Result> {
+    execute(query: Query): Promise<Result>;
+}
+export interface ICommandHandler<Command, Result> {
+    execute(data: Command): Promise<Result>;
+}
+export interface IUseCase<CreateDTO, UpdateDTO, Entity, Condition> {
+    create(data: CreateDTO): Promise<Entity>;
+    execute(query: { pagination: Pagination, condition: Condition }): Promise<{ entities: Entity[], pagination: Pagination }>;
+    getById(id: string): Promise<Entity>;
+    update(id: string, data: UpdateDTO): Promise<Entity>;
+    delete(id: string): Promise<void>;
+}
