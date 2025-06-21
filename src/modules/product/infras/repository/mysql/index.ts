0d7c65bee3946a922122d74e8123d981e82f154b
thanhhoa3514 <ngothanhhoa3514@gmail.com>
2025-06-21 18:00:10 +0700

    add : create a product persistence model mapping product table

---
+++ src/modules/product/infras/repository/mysql/index.ts
@@ -0,0 +1,86 @@
+import { DataTypes, Model, Sequelize } from "sequelize";
+import { ProductStatus } from "../../../model/product-enum";
+
+export class ProductPersistence extends Model {
+    declare id: string;
+    declare status: ProductStatus;
+}
+export class CategoryPersistence extends Model { }
+export class BrandPersistence extends Model { }
+export const modelName: string = "Product";
+export function init(sequelize: Sequelize) {
+    ProductPersistence.init(
+        {
+            id: {
+                type: DataTypes.UUID,
+
+                primaryKey: true,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            images: {
+                type: DataTypes.JSON,
+                allowNull: true,
+            },
+            colors: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            price: {
+                type: DataTypes.DECIMAL(10, 2),
+                allowNull: false,
+            },
+            salePrice: {
+                type: DataTypes.DECIMAL(10, 2),
+                allowNull: true,
+                field: "sale_price",
+            },
+            description: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+            },
+            content: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+            },
+            rating: {
+                type: DataTypes.FLOAT,
+                allowNull: true,
+            },
+            saleCount: {
+                type: DataTypes.INTEGER,
+                allowNull: true,
+                field: "sale_count",
+            },
+            brandId: {
+                type: DataTypes.UUID,
+                allowNull: true,
+                field: "brand_id",
+            },
+            categoryId: {
+                type: DataTypes.UUID,
+                allowNull: false,
+                field: "category_id",
+            },
+            status: {
+                type: DataTypes.ENUM("active", "inactive", "deleted", "banned", "sold"),
+                allowNull: false,
+                defaultValue: "active",
+                validate: {
+                    isIn: [["active", "inactive", "deleted", "banned", "sold"]],
+                },
+            },
+
+        }, {
+        sequelize,
+        modelName,
+        tableName: "products",
+        timestamps: true,
+        underscored: true,
+        updatedAt: "updated_at",
+        createdAt: "created_at",
+    });
+    return ProductPersistence;
+}
