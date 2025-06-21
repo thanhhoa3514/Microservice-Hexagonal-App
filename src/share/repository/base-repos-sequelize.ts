9dc731c829d4f8e12d5dea1c6311f5322f25faa0
thanhhoa3514 <ngothanhhoa3514@gmail.com>
2025-06-21 17:57:49 +0700

    add : create a shared sequelize repository

--- src/share/repository/base-repos-sequelize.ts
+++ src/share/repository/base-repos-sequelize.ts
@@ -1,24 +1,52 @@
-import { IRepository } from "../interface";
+import { ICommandRepository, IQueryRepository, IRepository } from "../interface";
 import { BaseError, Op, Sequelize } from "sequelize";
 import { Pagination, PaginationSchema } from "../model/paging";
 import { EntityStatus } from "../model/mode-status";
 
-export class BaseRepositorySequelize<Entity, Condition, UpdateDTO> implements IRepository<Entity, Condition, UpdateDTO> {
-    constructor(protected readonly sequelize: Sequelize, protected readonly modelName: string) {
+export abstract class BaseRepositorySequelize<Entity, Condition, UpdateDTO> implements IRepository<Entity, Condition, UpdateDTO> {
+    constructor(
+        readonly queryRepository: BaseQueryRepositorySequelize<Entity, Condition>,
+        readonly commandRepository: BaseCommandRepositorySequelize<Entity, UpdateDTO>
+    ) {
     }
+    async getById(id: string): Promise<Entity> {
+        return await this.queryRepository.getById(id);
+    }
+    async getAll(pagination: Pagination, condition: Condition): Promise<{ entities: Entity[]; pagination: Pagination; }> {
+        return await this.queryRepository.getAll(pagination, condition);
+    }
+    async count(condition: Condition): Promise<number> {
+        return await this.queryRepository.count(condition);
+    }
+    async findByCondition(condition: Condition): Promise<Entity | null> {
+        return await this.queryRepository.findByCondition(condition);
+    }
+    async update(id: string, data: UpdateDTO): Promise<void> {
+        await this.commandRepository.update(id, data);
+    }
+    async delete(id: string): Promise<void> {
+        await this.commandRepository.delete(id);
+
+    }
+    async insert(data: Entity): Promise<void> {
+        await this.commandRepository.insert(data);
+    }
+}
+export abstract class BaseQueryRepositorySequelize<Entity, Condition> implements IQueryRepository<Entity, Condition> {
+    constructor(protected readonly sequelize: Sequelize, protected readonly modelName: string) { }
     async getById(id: string): Promise<Entity> {
         const entity = await this.sequelize.models[this.modelName].findByPk(id);
         if (!entity) {
             throw new Error(`${this.modelName} not found`);
         }
-
-        const persistenceData = entity.get({ plain: true });
-
-        return {
-            ...persistenceData,
-            created_at: persistenceData.createdAt,
-            updated_at: persistenceData.updatedAt
-        } as Entity;
+        return entity.get({ plain: true }) as Entity;
+    }
+    async findByCondition(condition: Condition): Promise<Entity | null> {
+        const entity = await this.sequelize.models[this.modelName].findOne({ where: condition as any });
+        if (!entity) {
+            return null;
+        }
+        return entity.get({ plain: true }) as Entity;
     }
     async getAll(pagination: Pagination, condition: Condition): Promise<{ entities: Entity[]; pagination: Pagination; }> {
         const { success, data, error } = PaginationSchema.safeParse(pagination);
@@ -50,28 +78,19 @@ export class BaseRepositorySequelize<Entity, Condition, UpdateDTO> implements IR
             pagination: { page, limit, total }
         }
     }
-    async findByCondition(condition: Condition): Promise<Entity | null> {
-        const entity = await this.sequelize.models[this.modelName].findOne({ where: condition as any });
-        if (!entity) {
-            return null;
-        }
-        const persistenceData = entity.get({ plain: true });
-        return persistenceData as Entity;
+    async count(condition: Condition): Promise<number> {
+        return await this.sequelize.models[this.modelName].count({ where: condition as any });
+    }
+}
+export abstract class BaseCommandRepositorySequelize<Entity, UpdateDTO> implements ICommandRepository<Entity, UpdateDTO> {
+    constructor(protected readonly sequelize: Sequelize, protected readonly modelName: string) { }
+    async insert(data: Entity): Promise<void> {
+        await this.sequelize.models[this.modelName].create(data as any);
     }
     async update(id: string, data: UpdateDTO): Promise<void> {
         await this.sequelize.models[this.modelName].update(data as any, { where: { id } });
     }
     async delete(id: string): Promise<void> {
-        const [affectedRows] = await this.sequelize.models[this.modelName].update(
-            { status: EntityStatus.DELETED },
-            { where: { id } }
-        );
-
-        if (affectedRows === 0) {
-            throw BaseError;
-        }
-    }
-    async insert(data: Entity): Promise<void> {
-        await this.sequelize.models[this.modelName].create(data as any);
+        await this.sequelize.models[this.modelName].destroy({ where: { id } });
     }
 }
